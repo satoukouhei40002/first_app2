7ccaf2a319d8fbe161063d29f471a4bec5f67ac8
satoukouhei40002 <a.k.o.o.z.vv@icloud.com>
2023-12-09 18:53:02 +0900

    ActiveRecordメソッド

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,6 @@
 class PostsController < ApplicationController
-<<<<<<< Updated upstream
 
-  def index  # indexアクションを定義した
-=======
   def index
-  @post = "これはコントローラーで定義したインスタンス変数を確認するための文字列です"
->>>>>>> Stashed changes
+  @posts = Post.all
   end
 end
