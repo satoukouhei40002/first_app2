04f5bd0c090773ff02b50a1a9fd46ee8878dd288
satoukouhei40002 <a.k.o.o.z.vv@icloud.com>
2023-12-09 10:34:18 +0900

    修正

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,10 @@
 class PostsController < ApplicationController
+<<<<<<< Updated upstream
 
   def index  # indexアクションを定義した
+=======
+  def index
+  @post = "これはコントローラーで定義したインスタンス変数を確認するための文字列です"
+>>>>>>> Stashed changes
   end
 end
